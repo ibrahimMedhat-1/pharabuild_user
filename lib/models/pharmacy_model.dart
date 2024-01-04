f2afa9fb3f29b1a709a65a2907205bc2e4966a04
ibrahimmedhat <doha.kroz0@gmail.com>
2024-01-05 01:08:09 +0300

    change categories data type && add doctor bio

--- lib/models/pharmacy_model.dart
+++ lib/models/pharmacy_model.dart
@@ -4,7 +4,7 @@ class PharmacyModel {
   final String pharmacyName;
   final String pharmacyNo;
   final String pharmacyAddress;
-  final List<Map<String,String>> categories;
+  final List<Map<String, String>> categories;
   final List<ReviewModel> reviews;
   PharmacyModel({
     required this.pharmacyName,
